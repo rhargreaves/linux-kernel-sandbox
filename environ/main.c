1738906fb8cbbf6bed4537acf3b7439afe45fa97
Robert Hargreaves <kiyouta@gmail.com>
2025-04-21 12:36:52 +0100

    Also try envp

--- environ/main.c
+++ environ/main.c
@@ -2,7 +2,7 @@
 
 extern char **environ;
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], char *envp[])
 {
 	char *endOfArgs = argv[argc - 1];
 	while (*endOfArgs != '\0')
@@ -13,7 +13,6 @@ int main(int argc, char *argv[])
 
 	char *env = (char *)endOfArgs;
 	printf("end of args: %p\n----------------\n", endOfArgs);
-
 	for (char *env = (char *)endOfArgs; *env != NULL; env++)
 	{
 		puts(env);
@@ -26,6 +25,13 @@ int main(int argc, char *argv[])
 
 	printf("environ: %p\n", environ);
 	for (char **env = environ; *env != NULL; env++)
+	{
+		puts(*env);
+	}
+	printf("----------------\n");
+
+	printf("envp: %p\n", envp);
+	for (char **env = envp; *env != NULL; env++)
 	{
 		puts(*env);
 	}
